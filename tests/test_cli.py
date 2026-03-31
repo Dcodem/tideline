03f50a51f4a31952a6f8b621cb2aaf52ec582644
Daniel Code McNeil <dancodemcneil@gmail.com>
2026-03-31 16:36:09 -0400

    test: cli list

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -5,3 +5,9 @@ def test_find_runs():
     from tideline.cli import main
     r = CliRunner().invoke(main, ['find', 'xyz'])
     assert r.exit_code == 0
+
+
+def test_list():
+    from click.testing import CliRunner
+    from tideline.cli import main
+    assert CliRunner().invoke(main, ['list']).exit_code == 0
