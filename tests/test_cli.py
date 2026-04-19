a99ff483ce5e29c282cc6f2e762e9a8e6ab3ff8d
Daniel Code McNeil <dancodemcneil@gmail.com>
2026-04-19 15:50:43 -0400

    test: `today` smoke

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -11,3 +11,9 @@ def test_list():
     from click.testing import CliRunner
     from tideline.cli import main
     assert CliRunner().invoke(main, ['list']).exit_code == 0
+
+
+def test_today():
+    from click.testing import CliRunner
+    from tideline.cli import main
+    assert CliRunner().invoke(main, ['today']).exit_code == 0
